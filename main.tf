0ab550728d36c48e4c30dd77025d6c74d78ac24f
Andrea Cipriani <andrea.cipriani@assist.it>
2022-01-07 17:19:00 +0100

    First commit

---
+++ main.tf
@@ -0,0 +1,436 @@
+data "aws_ecr_repository" "service_repository" {
+  name = var.service.name
+}
+data "aws_region" "current" {}
+
+data "aws_sns_topic" "system-alarm" {
+  count = var.alarm_topic_name == null ? 0 : 1
+  name  = var.alarm_topic_name
+}
+
+data "aws_service_discovery_dns_namespace" "service_registry" {
+  count = var.service_registry_name != null ? 1 : 0
+  name  = var.service_registry_name
+  type  = "DNS_PRIVATE"
+}
+
+data "aws_iam_policy" "task_execution_role_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+data "aws_iam_policy" "aws_service_autoscale_role" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceAutoscaleRole"
+}
+
+resource "aws_iam_role" "task_execution_role" {
+  name        = "task-execution-role"
+  description = "Allow containers agent to communicate with the cluster"
+  tags        = {
+    Name = "task-execution-role"
+  }
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "task_execution_role_attach" {
+  role       = aws_iam_role.task_execution_role.name
+  policy_arn = data.aws_iam_policy.task_execution_role_policy.arn
+}
+
+resource "aws_iam_role" "task_role" {
+  name        = "task-role"
+  description = "Allow containers to communicate with CloudWatch"
+  tags        = {
+    Name = "task-role"
+  }
+
+  assume_role_policy = jsonencode({
+    version : "2012-10-17",
+    statement : [
+      {
+        action : "sts:AssumeRole",
+        principal : {
+          service : "ecs-tasks.amazonaws.com"
+        },
+        effect : "Allow"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "task_role_policy" {
+  name = "task-policy"
+  role = aws_iam_role.task_role.id
+
+  policy = jsonencode({
+
+    version : "2012-10-17",
+    statement : [
+      {
+        effect : "Allow",
+        action : [
+          "xray:PutTelemetryRecords",
+          "xray:PutTraceSegments"
+        ],
+        resources : "*"
+      }
+    ]
+  })
+}
+
+resource "aws_service_discovery_service" "ecs_discovery_service" {
+  count = var.service_registry_name != null ? 1 : 0
+  name  = var.service.name
+
+  dns_config {
+    namespace_id   = data.aws_service_discovery_dns_namespace.service_registry[0].id
+    routing_policy = "WEIGHTED"
+
+    dns_records {
+      ttl  = 10
+      type = "A"
+    }
+  }
+
+  health_check_custom_config {
+    failure_threshold = 1
+  }
+}
+
+resource "aws_cloudwatch_log_group" "task_log_group" {
+  name              = "/ecs/${var.service.name}-log-group"
+  retention_in_days = 60
+}
+
+resource "aws_ecs_task_definition" "service_task_definition" {
+  container_definitions    = jsonencode([
+    {
+      name             = var.service.name
+      cpu              = var.service_params.cpu
+      memory           = var.service_params.memory
+      environment      = var.service.env
+      essential        = true
+      image            = var.service.image
+      tags             = []
+      portMappings     = [
+        {
+          containerPort = var.service.port
+          hostPort      = var.service.port
+          protocol      = "tcp"
+        }
+      ]
+      startTimeout     = 10
+      stopTimeout      = 10
+      healthCheck      = {
+        command  = ["curl -f http://localhost:${var.service.port}/${var.service.healthcheck_path} || exit 1"]
+        interval = 30
+        timeout  = 10
+        retries  = 5
+      }
+      logConfiguration = {
+        logDriver = "awslogs"
+        options   = {
+          awslogs-group         = aws_cloudwatch_log_group.task_log_group.name
+          awslogs-region        = data.aws_region.current.name
+          awslogs-stream-prefix = "ecs"
+        }
+      }
+    }
+  ])
+  family                   = var.service.name
+  network_mode             = "awsvpc"
+  cpu                      = var.service_params.cpu
+  memory                   = var.service_params.memory
+  execution_role_arn       = aws_iam_role.task_execution_role.arn
+  task_role_arn            = aws_iam_role.task_role.arn
+  requires_compatibilities = [
+    "FARGATE"
+  ]
+}
+
+data "aws_subnets" "subnets" {
+  filter {
+    name   = "vpc-id"
+    values = [var.vpc_id]
+  }
+  filter {
+    name   = "tag:Type"
+    values = [var.service_params.is_public ? "Public" : "Private"]
+  }
+}
+
+data "aws_lb" "lb_trigger" {
+  count = try(var.trigger.lb, null) != null ? 1 : 0
+  arn   = var.trigger.lb.arn
+}
+
+
+resource "aws_security_group" "service_sg" {
+  name   = "${var.service.name}-service-sg"
+  vpc_id = var.vpc_id
+
+  ingress {
+    protocol        = "tcp"
+    from_port       = var.service.port
+    to_port         = var.service.port
+    security_groups = var.trigger.lb != null ? data.aws_lb.lb_trigger[0].security_groups : null
+    cidr_blocks     = var.trigger.lb != null ? null : []
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.service.name}-service-sg"
+  }
+}
+
+resource "aws_ecs_service" "service" {
+
+  name                               = var.service.name
+  cluster                            = var.existing_cluster_name
+  task_definition                    = aws_ecs_task_definition.service_task_definition.arn
+  desired_count                      = var.service_params.desired_count
+  deployment_minimum_healthy_percent = 100
+  deployment_maximum_percent         = 200
+  force_new_deployment               = true
+
+  capacity_provider_strategy {
+    capacity_provider = var.service.capacity_provider.provider
+    base              = var.service.capacity_provider.base
+    weight            = var.service.capacity_provider.weight
+  }
+
+  network_configuration {
+    assign_public_ip = var.service_params.is_public
+    subnets          = data.aws_subnets.subnets.id
+    security_groups  = [aws_security_group.service_sg.id]
+  }
+
+  service_registries {
+    registry_arn = aws_service_discovery_service.ecs_discovery_service[0].arn
+  }
+
+  dynamic "load_balancer" {
+    for_each = try(var.trigger.lb, null) == null ? [] : aws_lb_target_group.service_tg[0]
+
+    content {
+      target_group_arn = load_balancer.value.arn
+      container_name   = var.service.name
+      container_port   = var.service.port
+    }
+  }
+
+  //tags = local.common_tags
+
+}
+
+resource "aws_cloudwatch_log_group" "service_log_group" {
+  depends_on        = [aws_ecs_service.service]
+  name              = "/ecs/${var.service.name}"
+  retention_in_days = 90
+}
+
+
+resource "aws_lb_target_group" "service_tg" {
+  count       = try(var.trigger.lb, null) == null ? 0 : 1
+  name        = "${var.service.name}-LBTG"
+  port        = var.service.port
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = var.vpc_id
+  health_check {
+    healthy_threshold = 3
+    path              = var.service.healthcheck_path
+    port              = var.service.port
+  }
+  stickiness {
+    type            = "lb_cookie"
+    cookie_duration = 1800
+  }
+}
+
+
+resource "aws_lb_listener_rule" "service" {
+  count        = try(var.trigger.lb, null) == null ? 0 : 1
+  listener_arn = data.aws_lb.lb_trigger[0].arn_suffix
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.service_tg[count.index].arn
+  }
+
+  condition {
+    path_pattern {
+      values = var.trigger.lb.conditions.path_patterns
+    }
+  }
+
+  dynamic "condition" {
+    for_each = length(try(var.trigger.lb.conditions.hosts, [])) > 0 ? [true] : []
+    content {
+      host_header {
+        values = var.trigger.lb.conditions.hosts
+      }
+    }
+  }
+
+  dynamic "condition" {
+    for_each = try(var.trigger.lb.conditions.http_headers, [])
+    content {
+      http_header {
+        http_header_name = condition.key
+        values           = condition.value
+      }
+    }
+  }
+
+  dynamic "condition" {
+    for_each = try(var.trigger.lb.conditions.http_methods, [])
+    content {
+      http_request_method {
+        values = var.trigger.lb.conditions.http_methods
+      }
+    }
+  }
+
+  dynamic "condition" {
+    for_each = try(var.trigger.lb.conditions.source_ips, [])
+    content {
+      source_ip {
+        values = var.trigger.lb.conditions.source_ips
+      }
+    }
+  }
+
+  dynamic "condition" {
+    for_each = try(var.trigger.lb.conditions.query_string, null) != null ? [true] : []
+    content {
+      query_string {
+        value  = var.trigger.lb.conditions.query_string
+      }
+    }
+  }
+
+}
+
+resource "aws_iam_role" "service_autoscaling_role" {
+  count       = var.service_autoscaling.scale_on_cpu || var.service_autoscaling.scale_on_memory ? 1 : 0
+  name        = "service-autoscaling-role"
+  description = "IAM role allowing ecs service to scale automatically"
+  path        = "/"
+  tags        = {
+    Name = "service-autoscaling-role"
+  }
+
+  assume_role_policy = jsonencode({
+    version : "2012-10-17",
+    statement : [
+      {
+        action : "sts:AssumeRole",
+        principal : {
+          service : "application-autoscaling.amazonaws.com"
+        },
+        effect : "Allow"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "service_autoscaling_role_policy_attach" {
+  count      = var.service_autoscaling.scale_on_cpu || var.service_autoscaling.scale_on_memory ? 1 : 0
+  role       = aws_iam_role.service_autoscaling_role[0].name
+  policy_arn = data.aws_iam_policy.aws_service_autoscale_role.arn
+}
+
+
+resource "aws_appautoscaling_target" "ecs_target" {
+  count = var.service_autoscaling.scale_on_cpu || var.service_autoscaling.scale_on_memory ? 1 : 0
+
+  depends_on         = [aws_ecs_service.service]
+  max_capacity       = var.service_autoscaling.max_instance_number
+  min_capacity       = var.service_params.desired_count
+  resource_id        = "service/${var.existing_cluster_name}/${aws_ecs_service.service.name}"
+  role_arn           = aws_iam_role.service_autoscaling_role[0].arn
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+}
+
+resource "aws_appautoscaling_policy" "cpu_scale_policy" {
+  count = var.service_autoscaling.scale_on_cpu ? 1 : 0
+
+  name               = "${var.service.name}-cpu-scale-policy"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.ecs_target[count.index].resource_id
+  scalable_dimension = aws_appautoscaling_target.ecs_target[count.index].scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ecs_target[count.index].service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
+    }
+
+    target_value       = var.service_autoscaling.cpu_threshold
+    scale_in_cooldown  = var.service_autoscaling.scale_in_cooldown
+    scale_out_cooldown = var.service_autoscaling.scale_out_cooldown
+
+  }
+}
+
+resource "aws_appautoscaling_policy" "memory_scale_policy" {
+  count = var.service_autoscaling.scale_on_memory ? 1 : 0
+
+  name               = "${var.service.name}-memory-scale-policy"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.ecs_target[count.index].resource_id
+  scalable_dimension = aws_appautoscaling_target.ecs_target[count.index].scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ecs_target[count.index].service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageMemoryUtilization"
+    }
+
+    target_value       = var.service_autoscaling.memory_threshold
+    scale_in_cooldown  = var.service_autoscaling.scale_in_cooldown
+    scale_out_cooldown = var.service_autoscaling.scale_out_cooldown
+
+  }
+}
+resource "aws_cloudwatch_metric_alarm" "service-alarm" {
+  count               = try(var.trigger.lb, null) != null && var.alarm_topic_name != null ? 1 : 0
+  alarm_name          = "${var.service.name}-lb-target-count-alarm"
+  evaluation_periods  = "2"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  threshold           = "0"
+  metric_name         = "HealthyHostCount"
+  namespace           = "AWS/ApplicationELB"
+  period              = "60"
+  statistic           = "Average"
+  treat_missing_data  = "missing"
+  alarm_description   = "Load Balancer target count of ${var.service.name} is 0"
+  alarm_actions       = [data.aws_sns_topic.system-alarm[count.index].arn]
+  dimensions          = {
+    TargetGroup  = aws_lb_target_group.service_tg[count.index].arn_suffix
+    LoadBalancer = data.aws_lb.lb_trigger[0].arn_suffix
+  }
+
+}
+
---
+++ variables.tf
@@ -0,0 +1,91 @@
+terraform {
+  experiments = [module_variable_optional_attrs]
+}
+
+variable "existing_cluster_name" {
+  type    = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "trigger" {
+  type    = object({
+    lb: object({
+      arn: string,
+      conditions: object({
+        path_patterns: list(string)
+        hosts: optional(list(string))
+        http_headers: optional(list(map(string)))
+        http_methods: optional(list(string))
+        source_ips: optional(list(string))
+        query_string: optional(string)
+      })
+    })
+  })
+  default = null
+}
+
+variable "service" {
+  type = object({
+    name : string
+    image : string
+    port : number
+    healthcheck_path : optional(string)
+    capacity_provider: object({
+      provider: string
+      base: number,
+      weight: number
+    })
+    env: map(string)
+  })
+}
+
+variable "service_registry_name" {
+  type = string
+  default = null
+}
+
+variable "service_params" {
+  description = "Service deployment parameters"
+  type        = object({
+    cpu           = number,
+    memory        = number,
+    desired_count = number,
+    is_public     = bool
+  })
+  default     = {
+    cpu           = 512,
+    memory        = 1024,
+    desired_count = 1,
+    is_public     = false
+  }
+}
+
+variable "service_autoscaling" {
+  description = "Service autoscaling parameters"
+  type        = object({
+    max_instance_number = number,
+    scale_on_cpu        = bool,
+    scale_on_memory     = bool,
+    cpu_threshold       = number,
+    memory_threshold    = number,
+    scale_out_cooldown  = number,
+    scale_in_cooldown   = number
+  })
+  default     = {
+    max_instance_number = 1,
+    scale_on_cpu        = false,
+    scale_on_memory     = false,
+    cpu_threshold       = 60,
+    memory_threshold    = 80,
+    scale_out_cooldown  = 60,
+    scale_in_cooldown   = 180
+  }
+}
+
+variable "alarm_topic_name" {
+  type    = string
+  default = null
+}
